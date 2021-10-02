1a07f35d0e384694bdd2c48687597dde3be40282
author <91764419+neupl@users.noreply.github.com>
2021-10-02 09:59:19 +0100

    Upload episode data.

---
+++ assets/js/selector.js
@@ -0,0 +1,427 @@
+function getSpec() {
+  return {
+    "$schema": "https://vega.github.io/schema/vega/v5.json",
+    "background": "white",
+    "padding": 5,
+    "height": 240,
+    "data": [
+    {"name": "heatmap_cell_store"},
+    {
+      "name": "data-847e7566856e15d22809a30281570099",
+      "values": [
+      {"home_id": 0, "away_id": 0, "payoff": 0},
+      {"home_id": 0, "away_id": 1, "payoff": -28.093610763549805},
+      {"home_id": 0, "away_id": 2, "payoff": 28.936542510986328},
+      {"home_id": 0, "away_id": 3, "payoff": -20.57371711730957},
+      {"home_id": 0, "away_id": 4, "payoff": 2.084261894226074},
+      {"home_id": 0, "away_id": 5, "payoff": -8.956621170043945},
+      {"home_id": 0, "away_id": 6, "payoff": -0.7056407928466797},
+      {"home_id": 0, "away_id": 7, "payoff": 0.7813715934753418},
+      {"home_id": 1, "away_id": 0, "payoff": 28.093610763549805},
+      {"home_id": 1, "away_id": 1, "payoff": 0},
+      {"home_id": 1, "away_id": 2, "payoff": -30},
+      {"home_id": 1, "away_id": 3, "payoff": -30},
+      {"home_id": 1, "away_id": 4, "payoff": -0.06221795082092285},
+      {"home_id": 1, "away_id": 5, "payoff": -30},
+      {"home_id": 1, "away_id": 6, "payoff": -28.283222198486328},
+      {"home_id": 1, "away_id": 7, "payoff": 7.205897331237793},
+      {"home_id": 2, "away_id": 0, "payoff": -28.936542510986328},
+      {"home_id": 2, "away_id": 1, "payoff": 30},
+      {"home_id": 2, "away_id": 2, "payoff": 0},
+      {"home_id": 2, "away_id": 3, "payoff": -29.601652145385742},
+      {"home_id": 2, "away_id": 4, "payoff": -24.433128356933594},
+      {"home_id": 2, "away_id": 5, "payoff": 3.7236452102661133},
+      {"home_id": 2, "away_id": 6, "payoff": -7.954130172729492},
+      {"home_id": 2, "away_id": 7, "payoff": -17.82071876525879},
+      {"home_id": 3, "away_id": 0, "payoff": 20.57371711730957},
+      {"home_id": 3, "away_id": 1, "payoff": 30},
+      {"home_id": 3, "away_id": 2, "payoff": 29.601652145385742},
+      {"home_id": 3, "away_id": 3, "payoff": 0},
+      {"home_id": 3, "away_id": 4, "payoff": -25.859853744506836},
+      {"home_id": 3, "away_id": 5, "payoff": 3.6775689125061035},
+      {"home_id": 3, "away_id": 6, "payoff": -14.41751480102539},
+      {"home_id": 3, "away_id": 7, "payoff": -12.663146018981934},
+      {"home_id": 4, "away_id": 0, "payoff": -2.084261894226074},
+      {"home_id": 4, "away_id": 1, "payoff": 0.06221795082092285},
+      {"home_id": 4, "away_id": 2, "payoff": 24.433128356933594},
+      {"home_id": 4, "away_id": 3, "payoff": 25.859853744506836},
+      {"home_id": 4, "away_id": 4, "payoff": 0},
+      {"home_id": 4, "away_id": 5, "payoff": -19.573726654052734},
+      {"home_id": 4, "away_id": 6, "payoff": -1.7983741760253906},
+      {"home_id": 4, "away_id": 7, "payoff": -4.310179710388184},
+      {"home_id": 5, "away_id": 0, "payoff": 8.956621170043945},
+      {"home_id": 5, "away_id": 1, "payoff": 30},
+      {"home_id": 5, "away_id": 2, "payoff": -3.7236452102661133},
+      {"home_id": 5, "away_id": 3, "payoff": -3.6775689125061035},
+      {"home_id": 5, "away_id": 4, "payoff": 19.573726654052734},
+      {"home_id": 5, "away_id": 5, "payoff": 0},
+      {"home_id": 5, "away_id": 6, "payoff": -16.1948184967041},
+      {"home_id": 5, "away_id": 7, "payoff": 0.26108455657958984},
+      {"home_id": 6, "away_id": 0, "payoff": 0.7056407928466797},
+      {"home_id": 6, "away_id": 1, "payoff": 28.283222198486328},
+      {"home_id": 6, "away_id": 2, "payoff": 7.954130172729492},
+      {"home_id": 6, "away_id": 3, "payoff": 14.41751480102539},
+      {"home_id": 6, "away_id": 4, "payoff": 1.7983741760253906},
+      {"home_id": 6, "away_id": 5, "payoff": 16.1948184967041},
+      {"home_id": 6, "away_id": 6, "payoff": 0},
+      {"home_id": 6, "away_id": 7, "payoff": -13.556225776672363},
+      {"home_id": 7, "away_id": 0, "payoff": -0.7813715934753418},
+      {"home_id": 7, "away_id": 1, "payoff": -7.205897331237793},
+      {"home_id": 7, "away_id": 2, "payoff": 17.82071876525879},
+      {"home_id": 7, "away_id": 3, "payoff": 12.663146018981934},
+      {"home_id": 7, "away_id": 4, "payoff": 4.310179710388184},
+      {"home_id": 7, "away_id": 5, "payoff": -0.26108455657958984},
+      {"home_id": 7, "away_id": 6, "payoff": 13.556225776672363},
+      {"home_id": 7, "away_id": 7, "payoff": 0}
+      ]
+    },
+    {
+      "name": "data-6ed0185df567905e1e3b566d701dc7db",
+      "values": [
+      {"home_id": 0, "away_id": 0, "graph": 0},
+      {"home_id": 0, "away_id": 1, "graph": 0},
+      {"home_id": 0, "away_id": 2, "graph": 0},
+      {"home_id": 0, "away_id": 3, "graph": 0},
+      {"home_id": 0, "away_id": 4, "graph": 0},
+      {"home_id": 0, "away_id": 5, "graph": 0},
+      {"home_id": 0, "away_id": 6, "graph": 0},
+      {"home_id": 0, "away_id": 7, "graph": 0},
+      {"home_id": 1, "away_id": 0, "graph": 1},
+      {"home_id": 1, "away_id": 1, "graph": 0},
+      {"home_id": 1, "away_id": 2, "graph": 0},
+      {"home_id": 1, "away_id": 3, "graph": 0},
+      {"home_id": 1, "away_id": 4, "graph": 0},
+      {"home_id": 1, "away_id": 5, "graph": 0},
+      {"home_id": 1, "away_id": 6, "graph": 0},
+      {"home_id": 1, "away_id": 7, "graph": 0},
+      {"home_id": 2, "away_id": 0, "graph": 0},
+      {"home_id": 2, "away_id": 1, "graph": 1},
+      {"home_id": 2, "away_id": 2, "graph": 0},
+      {"home_id": 2, "away_id": 3, "graph": 0},
+      {"home_id": 2, "away_id": 4, "graph": 0},
+      {"home_id": 2, "away_id": 5, "graph": 0},
+      {"home_id": 2, "away_id": 6, "graph": 0},
+      {"home_id": 2, "away_id": 7, "graph": 0},
+      {"home_id": 3, "away_id": 0, "graph": 0.4138914942741394},
+      {"home_id": 3, "away_id": 1, "graph": 0.29520657658576965},
+      {"home_id": 3, "away_id": 2, "graph": 0.2909019887447357},
+      {"home_id": 3, "away_id": 3, "graph": 0},
+      {"home_id": 3, "away_id": 4, "graph": 0},
+      {"home_id": 3, "away_id": 5, "graph": 0},
+      {"home_id": 3, "away_id": 6, "graph": 0},
+      {"home_id": 3, "away_id": 7, "graph": 0},
+      {"home_id": 4, "away_id": 0, "graph": 0},
+      {"home_id": 4, "away_id": 1, "graph": 0},
+      {"home_id": 4, "away_id": 2, "graph": 0},
+      {"home_id": 4, "away_id": 3, "graph": 1},
+      {"home_id": 4, "away_id": 4, "graph": 0},
+      {"home_id": 4, "away_id": 5, "graph": 0},
+      {"home_id": 4, "away_id": 6, "graph": 0},
+      {"home_id": 4, "away_id": 7, "graph": 0},
+      {"home_id": 5, "away_id": 0, "graph": 0},
+      {"home_id": 5, "away_id": 1, "graph": 0.3634306788444519},
+      {"home_id": 5, "away_id": 2, "graph": 0.00724733155220747},
+      {"home_id": 5, "away_id": 3, "graph": 0},
+      {"home_id": 5, "away_id": 4, "graph": 0.6293219923973083},
+      {"home_id": 5, "away_id": 5, "graph": 0},
+      {"home_id": 5, "away_id": 6, "graph": 0},
+      {"home_id": 5, "away_id": 7, "graph": 0},
+      {"home_id": 6, "away_id": 0, "graph": 0},
+      {"home_id": 6, "away_id": 1, "graph": 0},
+      {"home_id": 6, "away_id": 2, "graph": 0},
+      {"home_id": 6, "away_id": 3, "graph": 0.41580039262771606},
+      {"home_id": 6, "away_id": 4, "graph": 0.12169650197029114},
+      {"home_id": 6, "away_id": 5, "graph": 0.4625031352043152},
+      {"home_id": 6, "away_id": 6, "graph": 0},
+      {"home_id": 6, "away_id": 7, "graph": 0},
+      {"home_id": 7, "away_id": 0, "graph": 0},
+      {"home_id": 7, "away_id": 1, "graph": 0},
+      {"home_id": 7, "away_id": 2, "graph": 0},
+      {"home_id": 7, "away_id": 3, "graph": 0},
+      {"home_id": 7, "away_id": 4, "graph": 0},
+      {"home_id": 7, "away_id": 5, "graph": 0},
+      {"home_id": 7, "away_id": 6, "graph": 1},
+      {"home_id": 7, "away_id": 7, "graph": 0}
+      ]
+    },
+    {
+      "name": "data_0",
+      "source": "data-847e7566856e15d22809a30281570099",
+      "transform": [
+      {
+        "type": "filter",
+        "expr": "isValid(datum[\"payoff\"]) && isFinite(+datum[\"payoff\"])"
+      }
+      ]
+    },
+    {
+      "name": "data_1",
+      "source": "data-6ed0185df567905e1e3b566d701dc7db",
+      "transform": [
+      {
+        "type": "filter",
+        "expr": "isValid(datum[\"graph\"]) && isFinite(+datum[\"graph\"])"
+      }
+      ]
+    }
+    ],
+    "signals": [
+    {"name": "childWidth", "value": 240},
+    {
+      "name": "unit",
+      "value": {},
+      "on": [
+      {"events": "mousemove", "update": "isTuple(group()) ? group() : unit"}
+      ]
+    },
+    {
+      "name": "heatmap_cell",
+      "update": "vlSelectionResolve(\"heatmap_cell_store\", \"union\")"
+    }
+    ],
+    "layout": {"padding": 20, "bounds": "full", "align": "each"},
+    "marks": [
+    {
+      "type": "group",
+      "name": "concat_0_group",
+      "style": "cell",
+      "encode": {
+        "update": {
+          "width": {"signal": "childWidth"},
+          "height": {"signal": "height"}
+        }
+      },
+      "signals": [
+      {
+        "name": "heatmap_cell_tuple",
+        "on": [
+        {
+          "events": [{"source": "scope", "type": "click"}],
+          "update": "datum && item().mark.marktype !== 'group' ? {unit: \"concat_0\", fields: heatmap_cell_tuple_fields, values: [(item().isVoronoi ? datum.datum : datum)[\"home_id\"], (item().isVoronoi ? datum.datum : datum)[\"away_id\"]]} : null",
+          "force": true
+        },
+        {
+          "events": [{"source": "scope", "type": "dblclick"}],
+          "update": "null"
+        }
+        ]
+      },
+      {
+        "name": "heatmap_cell_tuple_fields",
+        "value": [
+        {"type": "E", "field": "home_id"},
+        {"type": "E", "field": "away_id"}
+        ]
+      },
+      {
+        "name": "heatmap_cell_modify",
+        "on": [
+        {
+          "events": {"signal": "heatmap_cell_tuple"},
+          "update": "modify(\"heatmap_cell_store\", heatmap_cell_tuple, true)"
+        }
+        ]
+      }
+      ],
+      "marks": [
+      {
+        "name": "concat_0_marks",
+        "type": "rect",
+        "style": ["rect"],
+        "interactive": true,
+        "from": {"data": "data_0"},
+        "encode": {
+          "update": {
+            "fill": {"scale": "concat_0_color", "field": "payoff"},
+            "opacity": [
+            {
+              "test": "!(length(data(\"heatmap_cell_store\"))) || (vlSelectionTest(\"heatmap_cell_store\", datum))",
+              "value": 1
+            },
+            {"value": 0.1}
+            ],
+            "x": {"scale": "concat_0_x", "field": "away_id"},
+            "width": {"scale": "concat_0_x", "band": 1},
+            "y": {"scale": "concat_0_y", "field": "home_id"},
+            "height": {"scale": "concat_0_y", "band": 1}
+          }
+        }
+      }
+      ],
+      "axes": [
+      {
+        "scale": "concat_0_x",
+        "orient": "bottom",
+        "grid": false,
+        "labelOverlap": true,
+        "labelAlign": "right",
+        "labelAngle": 270,
+        "labelBaseline": "middle",
+        "zindex": 1
+      },
+      {
+        "scale": "concat_0_y",
+        "orient": "left",
+        "grid": false,
+        "labelOverlap": true,
+        "zindex": 1
+      }
+      ],
+      "legends": [
+      {
+        "fill": "concat_0_color",
+        "gradientLength": {"signal": "clamp(height, 64, 200)"},
+        "symbolType": "square",
+        "title": "payoff",
+        "encode": {"gradient": {"update": {"opacity": {"value": 1}}}}
+      }
+      ]
+    },
+    {
+      "type": "group",
+      "name": "concat_1_group",
+      "style": "cell",
+      "encode": {
+        "update": {
+          "width": {"signal": "childWidth"},
+          "height": {"signal": "height"}
+        }
+      },
+      "signals": [
+      {
+        "name": "heatmap_cell_tuple",
+        "on": [
+        {
+          "events": [{"source": "scope", "type": "click"}],
+          "update": "datum && item().mark.marktype !== 'group' ? {unit: \"concat_1\", fields: heatmap_cell_tuple_fields, values: [(item().isVoronoi ? datum.datum : datum)[\"home_id\"], (item().isVoronoi ? datum.datum : datum)[\"away_id\"]]} : null",
+          "force": true
+        },
+        {
+          "events": [{"source": "scope", "type": "dblclick"}],
+          "update": "null"
+        }
+        ]
+      },
+      {
+        "name": "heatmap_cell_tuple_fields",
+        "value": [
+        {"type": "E", "field": "home_id"},
+        {"type": "E", "field": "away_id"}
+        ]
+      },
+      {
+        "name": "heatmap_cell_modify",
+        "on": [
+        {
+          "events": {"signal": "heatmap_cell_tuple"},
+          "update": "modify(\"heatmap_cell_store\", heatmap_cell_tuple, true)"
+        }
+        ]
+      }
+      ],
+      "marks": [
+      {
+        "name": "concat_1_marks",
+        "type": "rect",
+        "style": ["rect"],
+        "interactive": true,
+        "from": {"data": "data_1"},
+        "encode": {
+          "update": {
+            "fill": {"scale": "concat_1_color", "field": "graph"},
+            "opacity": [
+            {
+              "test": "!(length(data(\"heatmap_cell_store\"))) || (vlSelectionTest(\"heatmap_cell_store\", datum))",
+              "value": 1
+            },
+            {"value": 0.1}
+            ],
+            "x": {"scale": "concat_1_x", "field": "away_id"},
+            "width": {"scale": "concat_1_x", "band": 1},
+            "y": {"scale": "concat_1_y", "field": "home_id"},
+            "height": {"scale": "concat_1_y", "band": 1}
+          }
+        }
+      }
+      ],
+      "axes": [
+      {
+        "scale": "concat_1_x",
+        "orient": "bottom",
+        "grid": false,
+        "labelOverlap": true,
+        "labelAlign": "right",
+        "labelAngle": 270,
+        "labelBaseline": "middle",
+        "zindex": 1
+      },
+      {
+        "scale": "concat_1_y",
+        "orient": "left",
+        "grid": false,
+        "labelOverlap": true,
+        "zindex": 1
+      }
+      ],
+      "legends": [
+      {
+        "title": "interaction graph",
+        "fill": "concat_1_color",
+        "gradientLength": {"signal": "clamp(height, 64, 200)"},
+        "symbolType": "square",
+        "encode": {"gradient": {"update": {"opacity": {"value": 1}}}}
+      }
+      ]
+    }
+    ],
+    "scales": [
+    {
+      "name": "concat_0_x",
+      "type": "band",
+      "domain": {"data": "data_0", "field": "away_id", "sort": true},
+      "range": [0, {"signal": "childWidth"}],
+      "paddingInner": 0,
+      "paddingOuter": 0
+    },
+    {
+      "name": "concat_0_y",
+      "type": "band",
+      "domain": {"data": "data_0", "field": "home_id", "sort": true},
+      "range": [0, {"signal": "height"}],
+      "paddingInner": 0,
+      "paddingOuter": 0
+    },
+    {
+      "name": "concat_0_color",
+      "type": "linear",
+      "domain": [-30, 30],
+      "range": {"scheme": "redyellowgreen"},
+      "interpolate": "hcl",
+      "zero": true
+    },
+    {
+      "name": "concat_1_x",
+      "type": "band",
+      "domain": {"data": "data_1", "field": "away_id", "sort": true},
+      "range": [0, {"signal": "childWidth"}],
+      "paddingInner": 0,
+      "paddingOuter": 0
+    },
+    {
+      "name": "concat_1_y",
+      "type": "band",
+      "domain": {"data": "data_1", "field": "home_id", "sort": true},
+      "range": [0, {"signal": "height"}],
+      "paddingInner": 0,
+      "paddingOuter": 0
+    },
+    {
+      "name": "concat_1_color",
+      "type": "linear",
+      "domain": [0, 1],
+      "range": "heatmap",
+      "interpolate": "hcl",
+      "zero": true
+    }
+    ]
+  }};
--- index.md
+++ index.md
@@ -2,17 +2,32 @@
 layout: default
 ---
 
-Text can be **bold**, _italic_, or ~~strikethrough~~.
+# Interactive Visualization
 
-[Link to another page](./another-page.html).
+Visualization of 8 distinct `running-with-scissors` strategies executed by the same `NeuPL` agent.
 
-There should be whitespace between paragraphs.
+> Click on a cell in the payoff matrix to visualize the corresponding gameplay. Double click to unselect.
 
-There should be whitespace between paragraphs. We recommend including a README, or a file with information about your project.
+<div id="demo" align='center'>
+<div id="selector-view"></div>
+<iframe id="episode-view" src="assets/data/episode_1_0_00.html" scrolling=no height='840' width='600' frameborder='0'></iframe>
+<script type="text/javascript">
+    var spec = getSpec();
+    var view = new vega.View(vega.parse(spec), {
+      renderer:  'canvas',
+      container: '#selector-view',
+      hover:     true
+    }).run();
 
-# Header 1
-
-This is a normal paragraph following a header. GitHub is a code hosting platform for version control and collaboration. It lets you and others work together on projects from anywhere.
+    const episode_view = document.getElementById("episode-view");
+    view.addEventListener('click', function(event, item) {
+      if (item != null) {
+        console.log('clicked', item.datum.home_id, item.datum.away_id);
+        episode_view.src = "assets/data/episode_" + item.datum.home_id + "_" + item.datum.away_id + "_00.html";
+      }
+    });
+</script>
+</div>
 
 ## Header 2
 
